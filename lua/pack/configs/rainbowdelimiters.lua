438237fe34f11e9f71edf0e3baff25f95e4e172c
caprain <cap15369@gmail.com>
2026-04-17 15:47:20 +0800

    调整懒加载策略和颜色值

--- lua/pack/configs/rainbowdelimiters.lua
+++ lua/pack/configs/rainbowdelimiters.lua
@@ -2,26 +2,31 @@
 local P = {
 	name = "rainbow-delimiters.nvim",
 }
-
+--
 -- 懒加载触发器
 vim.api.nvim_create_autocmd({
-	"UIEnter", -- vim.schedule(function()
+	"FileType",
 }, {
 	callback = function()
-		vim.schedule(function()
 			PackUtils.load(P, function()
+				vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#E06C75" })
+				vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#E5C07B" })
+				vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#61AFEF" })
+				vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#D19A66" })
+				vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#98C379" })
+				vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#C678DD" })
+				vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#56B6C2" })
 				require("rainbow-delimiters.setup").setup({
 					highlight = {
-						"RainbowDelimiterBlue",
-						"RainbowDelimiterViolet",
-						"RainbowDelimiterRed",
-						"RainbowDelimiterYellow",
-						"RainbowDelimiterGreen",
-						"RainbowDelimiterOrange",
-						"RainbowDelimiterCyan",
+						'RainbowDelimiterBlue',
+						'RainbowDelimiterViolet',
+						'RainbowDelimiterRed',
+						'RainbowDelimiterYellow',
+						'RainbowDelimiterGreen',
+						'RainbowDelimiterOrange',
+						'RainbowDelimiterCyan',
 					},
 				})
 			end)
-		end)
 	end
 })
